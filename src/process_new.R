f4a6b655aec23acbe377e58231a137e8a9b20db7
Sho <sho.ohata@berkeley.edu>
2017-08-08 11:52:04 -0400

    file path mod`

--- src/process_new.R
+++ src/process_new.R
@@ -6,9 +6,11 @@ source(file.path(getwd(),'src','helper_functions.R'))
 #prev_date <- function(x) format(7 * floor(as.numeric(x-8+4) / 7) + as.Date(1-4,origin = "1970-01-01"), "%y%m%d")
 prev_date <- function(x) format(x-7, "%y%m%d")
 
-raw_data_list <- list.files(file.path(getwd(), "data", "raw_data"))
+raw_data_path <- file.path(getwd(), "data", "raw_data")
+raw_data_list <- list.files(raw_data_path)[grepl("(?=.*turnstile)(?=.*txt)", list.files(raw_data_path), perl =TRUE)]
 raw_data_list <- raw_data_list[order(raw_data_list)]
-processed_data_list <- list.files(file.path(getwd(), "data", "processed"))
+processed_data_path <- file.path(getwd(), "data", "processed")
+processed_data_list <- list.files(processed_data_path)[grepl("(?=.*processed)(?=.*csv)", list.files(processed_data_path), perl =TRUE)]
 
 unprocessed_list <- raw_data_list[!(substring(raw_data_list,11,16) %in% substring(processed_data_list, 1,6))]
 unprocessed_list <- unprocessed_list[order(unprocessed_list)]
