2c1fa4050bb00b422ca029937fff68685e923869
Sho <sho.ohata@berkeley.edu>
2017-08-08 12:11:34 -0400

    fixes

--- src/process_new.R
+++ src/process_new.R
@@ -1,7 +1,7 @@
 library(data.table)
 
 source(file.path(getwd(),'src','helper_functions.R'))
-
+max_interval <- c("20:30:00", "21:00:00", "21:30:00", "22:00:00", "22:30:00", "23:00:00", "23:30:00")
 ## Function to determine previous file
 #prev_date <- function(x) format(7 * floor(as.numeric(x-8+4) / 7) + as.Date(1-4,origin = "1970-01-01"), "%y%m%d")
 prev_date <- function(x) format(x-7, "%y%m%d")
@@ -20,7 +20,7 @@ for (file_name in unprocessed_list) {
   prev_file_path <- file.path(getwd(),"data","processed",prev_file_name)
   if (file.exists(prev_file_path)) {
     prev_data <- fread(prev_file_path)
-    last_reading <- prev_data[DATE==max(all[,DATE])][TIME %in% max_interval]
+    last_reading <- prev_data[DATE==max(prev_data[,DATE])][INTERVAL %in% max_interval]
     last_reading <- format_data(file_name, last_reading)
   } else {
     print(paste("Previous File:", prev_file_name, "not found.", sep = ' '))
